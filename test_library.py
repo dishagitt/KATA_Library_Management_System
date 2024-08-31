f522051a3e286ca5400585f93e9f5799db881e4f
Disha Prajapati <dishap277@gmail.com>
2024-08-31 15:27:18 +0530

    passed 1st testcase by creating a add_book() function in library.py file

--- test_library.py
+++ test_library.py
@@ -1,7 +1,9 @@
 import unittest
+from library import add_book
 
 class TestLibraryManagementSystem(unittest.TestCase):
-        
+    
+    # testcase for add_book() function    
     def test_add_book(self):
         self.assertIsNotNone(add_book())
         
