8ebdd282f3964b81d7efcceb00b9774d1f3d10d4
Disha Prajapati <dishap277@gmail.com>
2024-08-31 19:59:24 +0530

    added testcase to borrow a book

--- test_library.py
+++ test_library.py
@@ -1,17 +1,21 @@
 import unittest
-from library import Book, Library, borrow_book
+from library import Book, Library
 class TestLibraryManagementSystem(unittest.TestCase):
+    
+    # this method is called before each testcase
+    def setUp(self):
+        self.library = Library()
+        self.book1 = Book(1234567890123,'book1','author1',2024)
     
     # testcase for add_book() function    
     def test_add_book(self):
-        # self.assertIsNotNone(add_book("abc"))
-        self.library = Library()
-        self.book1 = Book(1234567890123,'book1','author1',2024)
+        # self.assertIsNotNone(add_book("abc"))       
         self.library.add_book(self.book1)
         
     def test_borrow_book(self):
         # self.assertIsNotNone(borrow_book())
-        self.assertIsNotNone(borrow_book(1234567890123))
+        # self.assertTrue(borrow_book(1234567890123))
+        self.library.borrow_book(4567890192345)
         
     
 if __name__ == '__main__':
