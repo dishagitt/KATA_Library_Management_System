1c874f967c71279fc10680aa48cba6f1b5531e93
Disha Prajapati <dishap277@gmail.com>
2024-08-31 17:36:39 +0530

    added a testcase to pass arguments to a book class

--- test_library.py
+++ test_library.py
@@ -5,7 +5,8 @@ class TestLibraryManagementSystem(unittest.TestCase):
     
     # testcase for add_book() function    
     def test_add_book(self):
-        self.assertIsNotNone(add_book("abc"))
+        # self.assertIsNotNone(add_book("abc"))
+        self.book1=Book(1234567890,'book1','author1',2024)
         
     
 if __name__ == '__main__':
